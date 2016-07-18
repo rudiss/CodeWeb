d17b421a74f83152cef21c485dbaac3e0f7e6109
Rudinei SIlva <sistema3@comunicareaparelhosauditivos.com>
2016-07-18 02:38:18 -0300

    styles change

---
+++ assets/js/js.js
@@ -0,0 +1,13 @@
+$(".toggleButton").hover(function() {
+
+  $(this).css({
+    backgroundColor: '#3B99FC',
+    color: 'white'
+  });
+}, function() {
+
+  $(this).css({
+    backgroundColor: '#EEEEEE',
+    color: 'black'
+  });
+});
